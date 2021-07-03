3094b04e0a3bb7e0c98e7d63a5be110d605d75a6
Shanmukeshwara Rao Gutti <33666906+sgutti@users.noreply.github.com>
2021-07-03 13:26:36 +0530

    Delete target/classes/db/migration directory

--- target/classes/db/migration/V3__role.sql
+++
@@ -1,8 +0,0 @@
-/*
-SQLyog Community v12.3.2 (64 bit)
-MySQL - 5.7.16-log 
-*********************************************************************
-*/
-/*!40101 SET NAMES utf8 */;
-
-insert into `ba_role` (`ROLE_ID`, `TENANT_ID`, `CREATED_BY`, `CREATED_DATE`, `MODIFIED_BY`, `MODIFIED_DATE`, `DESCRIPTION`, `NAME`) values('1','-1','sgutti','2017-03-16 15:16:55',NULL,NULL,'Administrator','Admin');
